81bb98cdd36c172760bba3bf2cd2eba2f7e380a5
wangxiangle <wang_x_le@163.com>
2018-12-07 14:54:54 +0800

    init

---
+++ app/src/main/java/com/test/http/httptest/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.test.http.httptest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import java.security.cert.X509Certificate
+import org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER
+import java.security.cert.CertificateException
+import javax.net.ssl.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var handler = Handler() {
+
+            when (it.what) {
+                0 -> {
+                    tv.text = it.obj as String
+                }
+            }
+            true
+        }
+        Thread {
+            kotlin.run {
+                var msg = Message.obtain()
+                msg.what = -1
+                msg.obj = "正在加载..."
+                handler.sendMessage(msg)
+
+                var httpURLConnection = URL("https://shshopping.online.sh.cn/culture/req.do").openConnection() as HttpsURLConnection
+
+                //todo
+                val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                    @Throws(CertificateException::class)
+                    override fun checkClientTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {
+                    }
+
+                    @Throws(CertificateException::class)
+                    override fun checkServerTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {
+                    }
+
+                    override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> {
+                        return arrayOf()
+                    }
+                })
+                val sslContext = SSLContext.getInstance("SSL")
+                sslContext.init(null, trustAllCerts, null)
+                httpURLConnection.sslSocketFactory = sslContext.socketFactory
+                httpURLConnection.hostnameVerifier = createHostCerifier()
+                httpURLConnection.connectTimeout = 5000
+                httpURLConnection.requestMethod = "GET"
+                httpURLConnection.readTimeout = 5000
+
+                var i = httpURLConnection.inputStream
+
+                var reader = BufferedReader(InputStreamReader(i))
+                var result = StringBuilder()
+                var line: String? = ""
+                try {
+                    do {
+                        result.append(line)
+                        line = reader.readLine()
+                    } while (line != null)
+
+                    var msg = Message.obtain()
+                    msg.what = 0
+                    msg.obj = result.toString()
+                    handler.sendMessage(msg)
+                } catch (ex: Exception) {
+                }
+            }
+        }.start()
+    }
+
+
+    fun createHostCerifier(): HostnameVerifier {
+
+        return HostnameVerifier { hostname, session ->
+            val hv = HttpsURLConnection.getDefaultHostnameVerifier()
+            hv.verify("shshopping.online.sh.cn", session)
+
+        }
+    }
+
+}
